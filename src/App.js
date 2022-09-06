7af95d2a3b729bfd16fe0c565277ed64591d4b38
Delmo, Lionel L. (ARC-TI) <lionel.delmo@nasa.gov>
2022-09-06 16:13:51 -0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App({ signOut }) {
         <div className="App">
             <Card>
                 <img src={logo} className="App-logo" alt="logo" />
-                <Heading level={1}>We now have Auth!</Heading>
+                <Heading level={1}>We now have Auth! testing</Heading>
             </Card>
             <Button onClick={signOut}>Sign Out</Button>
         </div>
